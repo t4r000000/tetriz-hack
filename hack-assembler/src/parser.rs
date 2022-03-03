6fb286d8f5a3eaa55ae44d7983d0af919e96a99f
ninginx <publictaro@gmail.com>
2022-03-03 22:17:51 +0900

    parser worked

--- hack-assembler/src/parser.rs
+++ hack-assembler/src/parser.rs
@@ -1,5 +1,6 @@
 use std::fs::File;
 use std::io::BufReader;
+use std::str::ParseBoolError;
 
 use crate::Statement;
 
@@ -19,7 +20,15 @@ impl Parser {
     //   }
     // }
 
-    pub fn parse_line(line: &str) -> Statement {
+    pub fn parse_lines(src: &str) -> Vec<Statement> {
+        let mut result: Vec<Statement> = Vec::new();
+        for line in Parser::select_effective_lines(src) {
+            result.push(Parser::parse_line(line));
+        }
+        result
+    }
+
+    fn parse_line<'a>(line: &'a str) -> Statement<'a> {
         if let Some('@') = line.chars().next() {
             Statement::new(
                 'A',
@@ -42,15 +51,46 @@ impl Parser {
             None
         };
         let mut line_comp = line_dest[0].split(';').collect::<Vec<&str>>();
-        println!("{:?}", line_comp);
+
         let jmp = if line_comp.len() > 1 {
             line_comp.pop()
         } else {
             None
         };
 
-        let comp = line_comp.pop();
+        fn wrap_str(s: &str) -> Option<&str> {
+            match s.len() {
+                0 => None,
+                _ => Some(s),
+            }
+        }
+        // Option<T>
+        // T -> Option<T> wrap_str
+        // https://hoverbear.org/blog/option-monads-in-rust/
+        // Maybe Monad in Haskell, bind operator >>=
+
+        let comp = line_comp.pop().and_then(wrap_str);
+
         Statement::new('C', None, comp, dest, jmp)
+    }
+
+    fn select_effective_lines<'a>(line: &'a str) -> Vec<&'a str> {
+        let mut effective_lines = Vec::new();
+        for ee in line.split('\n') {
+            let trimed = Parser::trim_line(ee);
+            if trimed.is_empty() {
+                continue;
+            }
+            effective_lines.push(trimed)
+        }
+        effective_lines
+    }
+    // trim comment(//), blanl(\t), cariage return(\r)
+    fn trim_line<'a>(line: &'a str) -> &'a str {
+        let trimed_space = line.trim(); // \t trim
+        let trimed_cr = trimed_space.trim_matches('\r'); // \t trim
+        let trimed_comment: Vec<&str> = trimed_cr.split("//").collect();
+        trimed_comment[0]
     }
 }
 
@@ -67,10 +107,38 @@ mod parser_tests {
             Parser::parse_line("@50"),
             Statement::new('A', Some(50), None, None, None)
         );
-        println!("{:?}", Parser::parse_line("D+A"));
         assert_eq!(
             Parser::parse_line("D+A"),
             Statement::new('C', None, Some("D+A"), None, None)
+        );
+        assert_eq!(
+            Parser::parse_line("D+A;JMP"),
+            Statement::new('C', None, Some("D+A"), None, Some("JMP"))
+        );
+        assert_eq!(
+            Parser::parse_line(";JMP"),
+            Statement::new('C', None, None, None, Some("JMP"))
+        );
+        assert_eq!(
+            Parser::select_effective_lines("M=D+A;JMP\n;JEQ"),
+            ["M=D+A;JMP", ";JEQ"]
+        );
+
+        assert_eq!(
+            Parser::select_effective_lines("\nM=D+A;JMP\n;JEQ"),
+            ["M=D+A;JMP", ";JEQ"]
+        );
+        assert_eq!(
+            Parser::select_effective_lines("//\nM=D+A;JMP\n;JEQ"),
+            ["M=D+A;JMP", ";JEQ"]
+        );
+        assert_eq!(
+            Parser::parse_lines("//\n@1\nM=D+A;JMP\n;JEQ"),
+            [
+                Statement::new('A', Some(1), None, None, None),
+                Statement::new('C', None, Some("D+A"), Some("M"), Some("JMP")),
+                Statement::new('C', None, None, None, Some("JEQ"))
+            ]
         )
     }
 }
