040094da0307988a58fb5d7e2c9d054b90f8fd68
ninginx <publictaro@gmail.com>
2022-03-21 10:32:57 +0900

    simple add working many be, at least in parser

--- hack-vm/src/parser.rs
+++ hack-vm/src/parser.rs
@@ -1,6 +1,7 @@
 use std::io::{BufRead, BufReader, Read};
 
-enum CommandType {
+#[derive(PartialEq, Debug)]
+pub enum CommandType {
     C_ARITHMETIC,
     C_PUSH,
     C_POP,
@@ -20,9 +21,9 @@ trait Parser {
     fn new<R: Read>(f: BufReader<R>) -> Self;
     fn has_more_commands(&self) -> bool;
     fn advance(&mut self) {}
-    // fn command_type() -> String;
-    // fn arg1() -> String;
-    // fn arg2() -> i32;
+    fn command_type(&self) -> CommandType;
+    fn arg1(&self) -> &str;
+    fn arg2(&self) -> i32;
 }
 
 impl Parser for Commands {
@@ -49,11 +50,35 @@ impl Parser for Commands {
     fn advance(&mut self) {
         self.current_idx += 1;
     }
+    fn command_type(&self) -> CommandType {
+        match self.cmds[self.current_idx][0].as_str() {
+            "push" => CommandType::C_PUSH,
+            "add" => CommandType::C_ARITHMETIC,
+            _ => panic!("undefined command type case"),
+        }
+    }
+
+    fn arg1(&self) -> &str {
+        match self.command_type() {
+            CommandType::C_ARITHMETIC => self.cmds[self.current_idx][0].as_str(),
+            CommandType::C_PUSH => self.cmds[self.current_idx][1].as_str(),
+            _ => panic!("undefined command type at arg1"),
+        }
+    }
+
+    fn arg2(&self) -> i32 {
+        match self.command_type() {
+            CommandType::C_PUSH => self.cmds[self.current_idx][2]
+                .parse()
+                .expect("invalid arg2,not int"),
+            _ => panic!("invalid command type at arg2"),
+        }
+    }
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::parser::{Commands, Parser};
+    use crate::parser::{CommandType, Commands, Parser};
     use std::io::{BufReader, Cursor};
 
     #[test]
@@ -84,14 +109,45 @@ mod tests {
         assert!(!result_false);
     }
 
-    fn test_cmdstype() {
-        let command = Cursor::new("push constant 7\n");
-        let readio = BufReader::new(command);
-        let mut commands = Commands::new(readio);
-        let result_true = commands.has_more_commands();
-        assert_eq!(result_true, true);
-        commands.advance();
-        let result_false = commands.has_more_commands();
-        assert_eq!(result_false, false);
+    #[test]
+    fn test_cmdstype_push() {
+        let command = BufReader::new(Cursor::new("push constant 7\n"));
+        let mut cmds = Commands::new(command);
+        cmds.advance();
+        let arthmethtic = cmds.command_type();
+        assert_eq!(CommandType::C_PUSH, arthmethtic);
+    }
+
+    #[test]
+    fn test_cmdstype_add() {
+        let command = BufReader::new(Cursor::new("add\n"));
+        let mut cmds = Commands::new(command);
+        cmds.advance();
+        let arthmethtic = cmds.command_type();
+        assert_eq!(CommandType::C_ARITHMETIC, arthmethtic);
+    }
+    #[test]
+    fn test_cmdstype_simple_add() {
+        let command = BufReader::new(Cursor::new("push constant 8\npush constant 7\nadd\n"));
+        let mut cmds = Commands::new(command);
+        cmds.advance();
+        let arthmethtic = cmds.command_type();
+        let arg1 = cmds.arg1();
+        let arg2 = cmds.arg2();
+        assert_eq!(CommandType::C_PUSH, arthmethtic);
+        assert_eq!("constant", arg1);
+        assert_eq!(8, arg2);
+        cmds.advance();
+        let arthmethtic = cmds.command_type();
+        let arg1 = cmds.arg1();
+        let arg2 = cmds.arg2();
+        assert_eq!(CommandType::C_PUSH, arthmethtic);
+        assert_eq!("constant", arg1);
+        assert_eq!(7, arg2);
+        cmds.advance();
+        let arthmethtic = cmds.command_type();
+        let arg1 = cmds.arg1();
+        assert_eq!(CommandType::C_ARITHMETIC, arthmethtic);
+        assert_eq!("add", arg1);
     }
 }
